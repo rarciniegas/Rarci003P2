fc3b9c9c95665a1ef3028b11bf2647ae2b168d7b
Roberto Arciniegas <robertoarciniegas@Robertos-MacBook-Air.local>
2015-09-08 17:49:40 -0400

    Initial Commit

---
+++ Rarci003P2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rarci003P2
+//
+//  Created by Roberto Arciniegas on 9/8/15.
+//  Copyright (c) 2015 Roberto Arciniegas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Rarci003P2Tests/Rarci003P2Tests.m
@@ -0,0 +1,40 @@
+//
+//  Rarci003P2Tests.m
+//  Rarci003P2Tests
+//
+//  Created by Roberto Arciniegas on 9/8/15.
+//  Copyright (c) 2015 Roberto Arciniegas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Rarci003P2Tests : XCTestCase
+
+@end
+
+@implementation Rarci003P2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
