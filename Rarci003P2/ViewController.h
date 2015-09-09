1dc76cc7b4598ca728ac345466f678af655b726d
Roberto Arciniegas <robertoarciniegas@Robertos-MacBook-Air.local>
2015-09-09 13:58:09 -0400

    first commit

--- Rarci003P2/ViewController.h
+++ Rarci003P2/ViewController.h
@@ -10,6 +10,29 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UILabel *display;
+
+-(void) processDigit: (int) digit;
+-(void) processOp: (char) theOp;
+-(void) storeFracPart;
+
+// Numeric keys
+
+-(IBAction) clickDigit: (UIButton *) sender;
+
+// Arithmetic Operation keys
+
+-(IBAction) clickPlus;
+-(IBAction) clickMinus;
+-(IBAction) clickMultiply;
+-(IBAction) clickDivide;
+
+// Misc. Keys
+
+-(IBAction) clickOver;
+-(IBAction) clickEquals;
+-(IBAction) clickClear;
+
 
 @end
 
--- Rarci003P2/ViewController.m
+++ Rarci003P2/ViewController.m
@@ -13,12 +13,73 @@
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+{
+    char op;
+    int currentNumber;
+    BOOL firstOperand, isNumerator;
+    Calculator *myCalculator;
+    NSMutableString *displayString;
 }
 
+@synthesize display;
+
+
+- (void)viewDidLoad {
+    // Override point for customization after application launch
+    firstOperand = YES;
+    isNumerator = YES;
+    displayString = [NSMutableString stringWithCapacity: 40];
+    myCalculator = [[Calculator alloc] init];
+}
+
+-(void) processDigit: (int) digit {
+    currentNumber = currentNumber * 10 + digit;
+    [displayString appendString:
+     [NSString stringWithFormat: @"%i", digit]];
+    display.text = displayString;
+}
+
+- (IBAction) clickDigit: (UIButton *) sender {
+    int digit = sender.tag;
+    [self processDigit: digit];
+}
+
+-(void) processOp: (char) theOp {
+    NSString *opStr;
+    op = theOp;
+    switch (theOp) { case '+':
+            opStr = @" + ";
+            break; case '-':
+            opStr = @" – ";
+            break; case '*':
+            opStr = @" × ";
+            break; case '/':
+            opStr = @" ÷ "; break;
+    }
+    [self storeFracPart];
+    firstOperand = NO;
+    isNumerator = YES;
+    [displayString appendString: opStr];
+    display.text = displayString;
+}
+
+-(void) storeFracPart {
+    if (firstOperand) {
+        if (isNumerator) {
+            myCalculator.operand1.numerator = currentNumber;
+            myCalculator.operand1.denominator = 1; // e.g. 3 * 4/5 = }
+            else
+                myCalculator.operand1.denominator = currentNumber;
+        }
+        else if (isNumerator) {
+            myCalculator.operand2.numerator = currentNumber;
+            myCalculator.operand2.denominator = 1; // e.g. 3/2 * 4 = }
+            else {
+                myCalculator.operand2.denominator = currentNumber; firstOperand = YES;
+            }
+            currentNumber = 0;
+        }
+    
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
